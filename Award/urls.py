5e6900d87483924651fc9827d3b15bf6f61f7369
colletemine <jepngetichcolleta@gmail.com>
2022-06-11 03:34:45 +0300

    add project views and url

--- Award/urls.py
+++ Award/urls.py
@@ -11,7 +11,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
   path('',views.home_page, name='homepage'),
-  path('search/', views.search_results, name='search_results')
+  path('search/', views.search_results, name='search_results'),
+  path('project', views.project, name='project')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- Award/views.py
+++ Award/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from Award.models import *
+from .forms import *
 # Create your views here.
 
 def home_page(request):
    return render(request,'homepage.html')
+
+
+def project(request):
+   current_user = request.user
+   if request.method == 'POST':
+      form = ProjectForm(request.POST, request.FILES)
+      if form.is_valid():
+         project = form.save(commit=False)
+         project.developer = current_user
+         project.save()
+
+   form = ProjectForm()
+   return render(request, 'project.html', {'form': form})
 
 def search_results(request):
 
